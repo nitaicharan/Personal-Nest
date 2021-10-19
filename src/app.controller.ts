9087300dd2570e9fb7ad8251853dbee610dba8b9
Nitai Charan <nitaicharan@gmail.com>
2021-10-19 00:00:00 -0300

    Operations
    https://docs.nestjs.com/openapi/operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,42 @@
-import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common';
-import { ApiBody, ApiQuery } from '@nestjs/swagger';
+import { applyDecorators, Body, Controller, Delete, Get, Patch, Post, Query, Type, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiCreatedResponse, ApiExtension, ApiExtraModels, ApiHeader, ApiOkResponse, ApiQuery, ApiTags, getSchemaPath } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { CatDto } from './cat.dto';
 import { CreateCatDto } from './create-cat.dto';
+import { FileUploadDto } from './file-upload.dto';
+import { PaginatedDto } from './paginated.dto';
 import { UserRole } from './user-role.enum';
 
-@Controller()
+const ApiPaginatedResponse = <TModel extends Type<any>>(
+  model: TModel,
+) => {
+  return applyDecorators(
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PaginatedDto) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+};
+
+@ApiTags('cats')
+@ApiHeader({
+  name: 'X-MyHeader',
+  description: 'Custom header'
+})
+@ApiExtraModels(PaginatedDto)
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
@@ -34,6 +66,52 @@ export class AppController {
       },
     },
   })
+
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: CreateCatDto
+  })
   @Patch()
   async create1(@Body() coords: number[][]) { }
+
+
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'List of cats',
+    type: FileUploadDto,
+  })
+  @Post('files')
+  uploadFile(@UploadedFile() file) { }
+
+  @ApiExtension('x-foo', { hello: 'world' })
+  @Post('test')
+  test() {
+  }
+
+  @ApiOkResponse({
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(PaginatedDto) },
+        {
+          properties: {
+            results: {
+              type: 'array',
+              items: { $ref: getSchemaPath(CatDto) },
+            },
+          },
+        },
+      ],
+    },
+  })
+  @Get('find_all')
+  async find_all(): Promise<PaginatedDto<CatDto>> {
+    return new Promise(() => true);
+  }
+
+
+  @ApiPaginatedResponse(CatDto)
+  async findAll(): Promise<PaginatedDto<CatDto>> {
+    return new Promise(() => true);
+  }
 }
