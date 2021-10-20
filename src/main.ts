82e4153329d72cf223cbca052f78667784b24689
Nitai Charan <nitaicharan@gmail.com>
2021-10-20 00:00:00 -0300

    Introduction
    https://docs.nestjs.com/openapi/types-and-parameters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+
+  const config = new DocumentBuilder()
+    .setTitle('Personal-Nest')
+    .setDescription('This project is part of my personal study about the framework for building efficient, reliable and scalable server-side applications Nest.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+
+  const APP_PORT = 3000;
+  await app.listen(APP_PORT);
+  console.log('Application listen on port: ', APP_PORT);
+})();
